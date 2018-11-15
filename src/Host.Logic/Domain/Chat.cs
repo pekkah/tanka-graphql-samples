75f5bec6bd75db3e2181bf29469c0e745a8299b4
Pekka Heikura <pekkah@gmail.com>
2018-11-15 13:22:43 +0200

    Starting to define the domain of Chat

--- src/Host.Logic/Domain/Chat.cs
+++ src/Host.Logic/Domain/Chat.cs
@@ -1,31 +1,65 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using fugu.graphql.type;
 
 namespace fugu.graphql.samples.Host.Logic.Domain
 {
     public class Chat
     {
         private List<Message> _messages = new List<Message>();
+        private List<Channel> _channels = new List<Channel>();
 
-        public Task PostMessageAsync(int channelId, InputMessage message)
+        public async Task PostMessageAsync(int channelId, InputMessage message)
         {
+            var channel = await GetChannelAsync(channelId);
+
+            if (channel == null)
+            {
+                throw new InvalidOperationException($"Channel '{channelId}' not found");
+            }
+
             _messages.Add(new Message()
             {
                 Content = message.Content
             });
-            return Task.CompletedTask;
+        }
+
+        private Task<Channel> GetChannelAsync(int channelId)
+        {
+            var channel = _channels.SingleOrDefault(c => c.Id == channelId);
+            return Task.FromResult(channel);
         }
 
         public IEnumerable<Message> GetMessagesAsync(int channelId, int latest = 100)
         {
             return _messages.ToArray();
         }
+
+        public Task CreateChannelAsync(InputChannel inputChannel)
+        {
+            var channel = new Channel()
+            {
+                Id = 1,
+                Name = inputChannel.Name
+            };
+
+            _channels.Add(channel);
+
+            return Task.CompletedTask;
+        }
     }
 
     public class InputMessage
     {
         public string Content { get; set; }
+    }
+
+    public class InputChannel
+    {
+        public string Name { get; set; }
     }
 
     public class Channel
--- tests/Host.Facts/ChatFacts.cs
+++ tests/Host.Facts/ChatFacts.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using fugu.graphql.samples.Host.Logic.Domain;
 using Xunit;
@@ -7,8 +8,33 @@ namespace Host.Facts
     public class ChatFacts
     {
         [Fact]
-        public async Task Post_Message_()
+        public async Task Post_message()
         {
+            /* Given */
+            var inputMessage = new InputMessage()
+            {
+                Content = "123"
+            };
+            var channelId = 1;
+            var sut = new Chat();
+            await sut.CreateChannelAsync(new InputChannel()
+            {
+                Name = "General"
+            });
+
+
+            /* When */
+            await sut.PostMessageAsync(channelId, inputMessage);
+
+            /* Then */
+            var messages = sut.GetMessagesAsync(channelId);
+            Assert.Single(messages, m => m.Content == inputMessage.Content);
+        }
+
+        [Fact]
+        public async Task Post_message_should_fail_if_no_channel()
+        {
+            /* Given */
             /* Given */
             var inputMessage = new InputMessage()
             {
@@ -19,11 +45,11 @@ namespace Host.Facts
 
 
             /* When */
-            await sut.PostMessageAsync(channelId, inputMessage);
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(
+                ()=> sut.PostMessageAsync(channelId, inputMessage));
 
             /* Then */
-            var messages = sut.GetMessagesAsync(channelId);
-            Assert.Single(messages, m => m.Content == inputMessage.Content);
+            Assert.NotNull(exception.Message);
         }
     }
 }
