7e26024eabaa9a80693433405d69bb49e6a85266
Pekka Heikura <pekkah@gmail.com>
2019-04-06 17:27:25 +0300

    Updated to use the server link from libs

--- src/Host/Links.cs
+++ src/Host/Links.cs
@@ -1,43 +1,31 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
-using tanka.graphql.language;
 using tanka.graphql.links;
-using tanka.graphql.requests;
 
 namespace tanka.graphql.samples.Host
 {
     public static class Links
     {
-        public static ExecutionResultLink Signalr(string url)
+        public static ExecutionResultLink SignalR(string url)
         {
-            return async (document, variables, token) =>
+            return RemoteLinks.Server(async cancellationToken =>
             {
                 var connection = new HubConnectionBuilder()
                     .WithUrl(url)
                     .Build();
 
-                await connection.StartAsync(token);
+                await connection.StartAsync(cancellationToken);
 
                 Task OnClosed(Exception e)
                 {
-                    return connection.StartAsync(token);
+                    return connection.StartAsync(cancellationToken);
                 }
 
                 connection.Closed += OnClosed;
-                token.Register(() => { connection.Closed -= OnClosed; });
-
-                var query = new QueryRequest
-                {
-                    Query = document.ToGraphQL(),
-                    Variables = variables != null
-                        ? new Dictionary<string, object>(variables)
-                        : null
-                };
-
-                return await connection.StreamQueryAsync(query, token);
-            };
+                cancellationToken.Register(() => connection.Closed -= OnClosed);
+                return connection;
+            });
         }
     }
 }
--- src/Host/Startup.cs
+++ src/Host/Startup.cs
@@ -31,7 +31,7 @@ namespace tanka.graphql.samples.Host
                 {
                     // create channelsSchema by introspecting channels service
                     var channelsBuilder = new SchemaBuilder();
-                    var channelsLink = Links.Signalr("https://localhost:5010/hubs/graphql");
+                    var channelsLink = Links.SignalR("https://localhost:5010/hubs/graphql");
                     channelsBuilder.ImportIntrospectedSchema(channelsLink).GetAwaiter().GetResult();
 
                     var channelsSchema = RemoteSchemaTools.MakeRemoteExecutable(
@@ -39,8 +39,8 @@ namespace tanka.graphql.samples.Host
                         channelsLink);
 
                     // create messagesSchema by introspecting messages service
-                    var messagesLink = Links.Signalr("https://localhost:5011/hubs/graphql");
-                    var messagesBuilder = new SchemaBuilder(); 
+                    var messagesLink = Links.SignalR("https://localhost:5011/hubs/graphql");
+                    var messagesBuilder = new SchemaBuilder();
                     messagesBuilder.ImportIntrospectedSchema(messagesLink).GetAwaiter().GetResult();
 
                     var messagesSchema = RemoteSchemaTools.MakeRemoteExecutable(
