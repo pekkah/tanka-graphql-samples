5a35ce595b1b3d776c9a81047eb10ab38ce34618
Pekka Heikura <pekkah@gmail.com>
2019-05-21 16:57:41 +0300

    Add http controller to channels and use http request for queries and mutations from host to channels (#52)

---
+++ src/Channels.Host/api/QueryController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using tanka.graphql.requests;
+
+namespace tanka.graphql.samples.channels.host.api
+{
+    [Authorize(Policy = "authorize")]
+    [Route("api/graphql")]
+    [ApiController]
+    public class QueryController : Controller
+    {
+        private readonly IEnumerable<IExtension> _extensions;
+
+        //todo: fix duplicate class name
+        private readonly IOptionsMonitor<server.ExecutionOptions> _optionsMonitor;
+
+        public QueryController(IOptionsMonitor<server.ExecutionOptions> optionsMonitor,
+            IEnumerable<IExtension> extensions)
+        {
+            _optionsMonitor = optionsMonitor;
+            _extensions = extensions;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] QueryRequest request)
+        {
+            var options = _optionsMonitor.CurrentValue;
+
+            var document = Parser.ParseDocument(request.Query);
+            var schema = await options.GetSchema(null);
+            var result = await Executor.ExecuteAsync(new ExecutionOptions
+            {
+                Document = document,
+                Schema = schema,
+                OperationName = request.OperationName,
+                VariableValues = request.Variables,
+                Extensions = _extensions.ToArray()
+            });
+
+            return Ok(result);
+        }
+    }
+}
--- src/Host/Links.cs
+++ src/Host/Links.cs
@@ -1,8 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Claims;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using GraphQLParser.AST;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
+using tanka.graphql.language;
 using tanka.graphql.links;
+using tanka.graphql.requests;
 
 namespace tanka.graphql.samples.Host
 {
@@ -28,6 +38,67 @@ namespace tanka.graphql.samples.Host
 
                 return Task.FromResult(connection);
             });
+        }
+
+        public static ExecutionResultLink SignalROrHttp(
+            string hubUrl,
+            string httpUrl,
+            IHttpContextAccessor accessor)
+        {
+            var signalr = RemoteLinks.Server(cancellationToken =>
+            {
+                var connection = new HubConnectionBuilder()
+                    .WithUrl(hubUrl, configure =>
+                    {
+                        configure.AccessTokenProvider = () =>
+                        {
+                            var token = accessor.HttpContext
+                                ?.User
+                                .FindFirstValue("access_token");
+
+                            return Task.FromResult(token);
+                        };
+                    })
+                    .Build();
+
+                return Task.FromResult(connection);
+            });
+
+            var http = RemoteLinks.Http(
+                httpUrl,
+                transformRequest: operation =>
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Post, operation.Url);
+                    request.Headers.Authorization = new AuthenticationHeaderValue(
+                        "Bearer",
+                        accessor.HttpContext?.User.FindFirstValue("access_token"));
+
+                    var query = new QueryRequest
+                    {
+                        Query = operation.Document.ToGraphQL(),
+                        Variables = operation.Variables != null
+                            ? new Dictionary<string, object>(operation.Variables)
+                            : null
+                    };
+                    var json = JsonConvert.SerializeObject(query);
+                    request.Content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    return request;
+                });
+
+            return async (document, variables, token) =>
+            {
+                var hasQueryOrMutation = document.Definitions.OfType<GraphQLOperationDefinition>()
+                    .Any(op => op.Operation != OperationType.Subscription);
+
+                if (hasQueryOrMutation)
+                {
+                    var result = await http(document, variables, CancellationToken.None);
+                    return result;
+                }
+
+                return await signalr(document, variables, token);
+            };
         }
     }
 }
