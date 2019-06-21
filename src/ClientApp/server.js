7bdc721661a0389e4cd2c86b151a4dcbda48a5c0
Pekka Heikura <pekkah@gmail.com>
2019-06-21 14:33:39 +0300

    Restore catch all /*

--- src/ClientApp/server.js
+++ src/ClientApp/server.js
@@ -26,7 +26,7 @@ app.get('/config.js', function(req, res) {
 });
 
 
-app.get('*', function(req, res) {
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
   });
 
