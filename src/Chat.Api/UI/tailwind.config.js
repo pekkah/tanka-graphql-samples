5961c694af7172b7d366152978b28f69723eee1f
Pekka Heikura <pekkah@gmail.com>
2023-11-26 09:23:57 +0200

    Delete SolidJS UI

--- src/Chat.Api/UI/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-}
-
