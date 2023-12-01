b15c61e866894c090cee147c893c920ba29217c0
Pekka Heikura <pekkah@gmail.com>
2023-12-01 15:10:26 +0200

    Layout changes (#554)

--- src/Chat.Api/UI2/src/pages/home/index.tsx
+++ src/Chat.Api/UI2/src/pages/home/index.tsx
@@ -1,6 +1,23 @@
+import { useSession } from "../../model";
+
 export default function Home() {
-	return <>
-	  <p>Hello</p>
-	</>;
-  }
-  
+  const session = useSession();
+  return (
+    <div class="hero bg-base-200 rounded-xl mx-3">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-bold">Hello there</h1>
+          <p class="py-6">
+            Simple chat application using Tanka GraphQL as backend with Preact
+            frontend.
+          </p>
+          {!session.value.isAuthenticated && (
+            <a class="btn btn-primary" href="/signin">
+              Login
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
